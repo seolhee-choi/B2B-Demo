3f0887a3b560c2711af74066dc89154371d24a57
minsng <tjdalstmd033@naver.com>
2025-06-12 13:38:11 +0900

    DB Connect Test

--- backend/src/main/java/com/example/backend/controller/TestController.java
+++ backend/src/main/java/com/example/backend/controller/TestController.java
@@ -1,4 +1,25 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.TestDto;
+import com.example.backend.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final TestService testService;
+
+    @GetMapping("/conn")
+    public ResponseEntity<List<TestDto>> getConnectCheck() {
+        return ResponseEntity.ok().body(testService.getConnectCheck());
+    }
+
 }
--- backend/src/main/java/com/example/backend/dto/TestDto.java
+++ backend/src/main/java/com/example/backend/dto/TestDto.java
@@ -1,4 +1,12 @@
 package com.example.backend.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @RequiredArgsConstructor
 public class TestDto {
+
+    private String name;
+    private String conn;
 }
