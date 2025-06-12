d73d6cbcdbb5bafca436ab96faa6d8575f4576b7
hyejin <obndo8539@naver.com>
2025-06-12 17:25:49 +0900

    메뉴 서비스 구조 생성

---
+++ backend/src/main/java/com/example/backend/controller/MenuController.java
@@ -0,0 +1,14 @@
+package com.example.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/menu")
+@RequiredArgsConstructor
+public class MenuController {
+
+
+
+}
