425d3afa317e35888f605b65b216133636708421
BobaeKim <bobae.kim@northstar.co.kr>
2025-06-12 17:40:55 +0900

    대시보드 차트 구현, 캘린더 추가

--- frontend/src/views/DashBoard.vue
+++ frontend/src/views/DashBoard.vue
@@ -1,16 +1,39 @@
 <script setup>
 
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import DashboardCalendar from "@/views/dashboardMgmt/DashboardCalendar.vue";
+import DashboardChart from "@/views/dashboardMgmt/DashboardChart.vue";
 </script>
 
 <template>
   <default-layout>
     <template v-slot:content>
       <h1>Dash Board</h1>
+      <div class="dashboard-container">
+        <DashboardChart class="chart"/>
+        <DashboardCalendar class="calendar"/>
+      </div>
     </template>
   </default-layout>
 </template>
 
 <style scoped>
+.dashboard-container {
+  display: flex;
+  flex-wrap: nowrap;
+  width: 100%;
+  gap: 0; /* 간격 없이 반반 */
+}
 
+.chart {
+  flex: 1;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.calendar {
+  flex: 1;
+  padding: 10px;
+  box-sizing: border-box;
+}
 </style>
---
+++ frontend/src/views/dashboardMgmt/DashboardCalendar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {onMounted, ref} from 'vue';
+import 'element-plus/es/components/calendar/style/css'
+import {ElCalendar} from 'element-plus'
+
+const value = ref(new Date());
+
+</script>
+
+<template>
+  <div class="calendar-container">
+  <h3>Calendar</h3>
+  <el-calendar v-model="value"/>
+  </div>
+</template>
+
+<style scoped>
+.calendar-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+</style>
