17bb004e085624902d7439c11e9f1d5336e73bd1
gochangeun <changeun.go@northstar.co.kr>
2025-06-16 16:41:51 +0900

    bookmark v.0.0.1 add SecurityConfig ApiResponse Change

---
+++ backend/src/main/java/com/example/backend/dto/BookMark_menu.java
@@ -0,0 +1,13 @@
+package com.example.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookMark_menu {
+
+  private Long userId;
+  private Long menuId;
+
+}
